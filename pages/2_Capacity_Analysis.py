f89192c41bf4d2ce9b10950c96aa7a5044bb3e82
brandjamie8 <brandjamie8@gmail.com>
2024-12-03 19:02:30 +0000

    Update 2_Capacity_Analysis.py

--- pages/2_Capacity_Analysis.py
+++ pages/2_Capacity_Analysis.py
@@ -186,7 +186,7 @@ if 'referral_df' in st.session_state and st.session_state.referral_df is not Non
             grand_total_df,
             x='Category',
             y='Appointments',
-            title='',
+            title=False,
             labels={'Appointments': 'Number of Appointments'},
             text='Appointments',
             color_discrete_sequence=px.colors.qualitative.Safe
